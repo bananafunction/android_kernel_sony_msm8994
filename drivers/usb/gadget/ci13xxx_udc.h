a866aa86dd6e3c07dd40bc49c46b466fd60a844f
Sujeet Kumar <ksujeet@codeaurora.org>
2015-05-26 17:04:54 +0530

    USB: ci13xxx_udc: Enable usb core and iface clock before reading registers
    Sysfs node is present for reading the dumping registers.
    But with usb cable disconnected if usb registers are dumped,
    usb core and iface clocks are off, it leads to unclocked
    access.
    
    Enable and disable thse clocks before and after dumping
    the registers so that unclocked access is not hit
    if registers are dumped with usb cable disconnected.
    
    Change-Id: I06f75749b8670b6331435137f65be20d5673b6c3
    Signed-off-by: Sujeet Kumar <ksujeet@codeaurora.org>

--- drivers/usb/gadget/ci13xxx_udc.h
+++ drivers/usb/gadget/ci13xxx_udc.h
@@ -152,6 +152,7 @@ struct ci13xxx_udc_driver {
 	bool (*cancel_pending_suspend)(struct ci13xxx *udc);
 	bool    (*in_lpm) (struct ci13xxx *udc);
 	void    (*set_fpr_flag) (struct ci13xxx *udc);
+	struct clk *pclk;
 };
 
 /* CI13XXX UDC descriptor & global resources */
@@ -180,6 +181,7 @@ struct ci13xxx {
 	int                        softconnect; /* is pull-up enable allowed */
 	unsigned long dTD_update_fail_count;
 	struct usb_phy            *transceiver; /* Transceiver struct */
+	struct clk                *pclk;
 	bool                      skip_flush; /* skip flushing remaining EP
 						upon flush timeout for the
 						first EP. */
