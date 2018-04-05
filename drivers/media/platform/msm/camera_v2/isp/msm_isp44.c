9cdc8f034885f60485cc3f4ee3fca4a2ae0acf9d
Sean Callanan <spyffe@google.com>
2018-04-04 17:35:19 -0700

    msm/camera_v2/isp: Handle use after free buffer
    In the code, start_fetch can try to access the
    buffer pointer variable after free, as the
    same pointer can be freed at RELEASE_BUF call
    at the same time.
    
    CRs-Fixed: 2149998
    Change-Id: Ic83f22336504cf67afe12131f791eee25477f011
    Signed-off-by: Meera Gande <mgande@codeaurora.org>
    Signed-off-by: Vijay Kumar TM <vtmuni@codeaurora.org>
    
    Bug: 69065862
    Signed-off-by: Sean Callanan <spyffe@google.com>
    [Resolved context conflicts]
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp44.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp44.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015, 2017-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -962,6 +962,7 @@ static int msm_vfe44_fetch_engine_start(struct vfe_device *vfe_dev,
 	ISP_DBG("%s: Fetch Engine ready\n", __func__);
 	buf->state = MSM_ISP_BUFFER_STATE_DIVERTED;
 	mutex_unlock(&vfe_dev->buf_mgr->lock);
+
 	return 0;
 }
 
