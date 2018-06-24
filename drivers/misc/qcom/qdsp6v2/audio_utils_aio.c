arter97/caf_msm-5.4

misc: qcom: qdsp6v2: use 32-bit variable to store token
Even when we send buffer address with more than 32-bit
to DSP, APR callback handle will return a token with only
32-bit. Hence use a 32-bit variable to store the buffer
address locally, so that comparison between these does
not fail.

Change-Id: I1ce574d57bb7ee2716d112711f6a160bdb6c3e22
Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>

drivers/misc/qcom/qdsp6v2/audio_utils_aio.c
@@ -290,7 +290,7 @@ void audio_aio_async_write_ack(struct q6audio_aio *audio, uint32_t token,
 			wake_up(&audio->write_wait);
 		}
 	} else {
-		pr_err("%s[%p]:expected=%lx ret=%x\n",
+		pr_err("%s[%p]:expected=%x ret=%x\n",
 			__func__, audio, used_buf->token, token);
 		spin_unlock_irqrestore(&audio->dsp_lock, flags);
 	}

drivers/misc/qcom/qdsp6v2/q6audio_v2_aio.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -182,7 +182,7 @@ void audio_aio_async_read_ack(struct q6audio_aio *audio, uint32_t token,
 	filled_buf = list_first_entry(&audio->in_queue,
 					struct audio_aio_buffer_node, list);
 
-	pr_debug("%s token: 0x[%d], filled_buf->token: 0x[%lu]",
+	pr_debug("%s token: 0x[%x], filled_buf->token: 0x[%x]",
 				 __func__, token, filled_buf->token);
 	if (token == (filled_buf->token)) {
 		list_del(&filled_buf->list);
@@ -212,7 +212,7 @@ void audio_aio_async_read_ack(struct q6audio_aio *audio, uint32_t token,
 					event_payload);
 		kfree(filled_buf);
 	} else {
-		pr_err("%s[%p]:expected=%lx ret=%x\n",
+		pr_err("%s[%p]:expected=%x ret=%x\n",
 			__func__, audio, filled_buf->token, token);
 		spin_unlock_irqrestore(&audio->dsp_lock, flags);
 	}

bananafunction/android_kernel_sony_msm8994

drivers/misc/qcom/qdsp6v2/audio_utils_aio.c
@@ -306,7 +306,7 @@ void audio_aio_async_write_ack(struct q6audio_aio *audio, uint32_t token,
 			wake_up(&audio->write_wait);
 		}
 	} else {
-		pr_err("%s[%pK]:expected=%lx ret=%x\n",
+		pr_err("%s[%pK]:expected=%x ret=%x\n",
 			__func__, audio, used_buf->token, token);
 		spin_unlock_irqrestore(&audio->dsp_lock, flags);
 	}

drivers/misc/qcom/qdsp6v2/q6audio_v2_aio.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, 2016 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -182,7 +182,7 @@ void audio_aio_async_read_ack(struct q6audio_aio *audio, uint32_t token,
 	filled_buf = list_first_entry(&audio->in_queue,
 					struct audio_aio_buffer_node, list);
 
-	pr_debug("%s token: 0x[%d], filled_buf->token: 0x[%lu]",
+	pr_debug("%s token: 0x[%x], filled_buf->token: 0x[%x]",
 				 __func__, token, filled_buf->token);
 	if (token == (filled_buf->token)) {
 		list_del(&filled_buf->list);
@@ -212,7 +212,7 @@ void audio_aio_async_read_ack(struct q6audio_aio *audio, uint32_t token,
 					event_payload);
 		kfree(filled_buf);
 	} else {
-		pr_err("%s[%pK]:expected=%lx ret=%x\n",
+		pr_err("%s[%pK]:expected=%x ret=%x\n",
 			__func__, audio, filled_buf->token, token);
 		spin_unlock_irqrestore(&audio->dsp_lock, flags);
 	}